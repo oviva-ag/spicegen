ed8be7508974eab2a00a23dddcd4f65a50356c90
Thomas Richner <thomas.richner@oviva.com>
2024-05-15 17:19:50 +0200

    Updated tests

---
+++ generator/src/test/java/com.oviva.spicegen.generator.internal/SpiceDbClientGeneratorImplTest.java
@@ -0,0 +1,51 @@
+package com.oviva.spicegen.generator.internal;
+
+import com.oviva.spicegen.model.ObjectDefinition;
+import com.oviva.spicegen.model.ObjectTypeRef;
+import com.oviva.spicegen.model.Relation;
+import com.oviva.spicegen.model.Schema;
+import com.oviva.spicegen.parser.SpiceDbSchemaParser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.File;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+class SpiceDbClientGeneratorImplTest {
+
+  @ParameterizedTest
+  @ValueSource(
+          strings = {
+//                  "files",
+                  "oviva",
+          })
+  void test() {
+
+    var generator = new SpiceDbClientGeneratorImpl();
+    var objectDefinition2 = new ObjectDefinition("foo", List.of(), List.of());
+    var objectDefinition1 =
+        new ObjectDefinition(
+            "hi",
+            List.of(new Relation("son_of", List.of(new ObjectTypeRef("foo", null)))),
+            List.of());
+    var definitions = List.of(objectDefinition1, objectDefinition2);
+    var schema = new Schema(definitions);
+    schema = loadSchema("oviva");
+    generator.generate(schema);
+  }
+
+  private Schema loadSchema(String name) {
+
+    var astInputStream =
+        this.getClass().getResourceAsStream("/fixtures/%s_ast.json".formatted(name));
+    var parser = new SpiceDbSchemaParser();
+
+    return parser.parse(astInputStream);
+  }
+}
