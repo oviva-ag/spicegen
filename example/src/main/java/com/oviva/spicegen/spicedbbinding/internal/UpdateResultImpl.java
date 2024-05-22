c0ffeefba15a8f9ec4ff36fe15cf09f36a4642b4
Thomas Richner <thomas.richner@oviva.com>
2024-05-22 18:01:26 +0200

    ARC-1699: Removed copy paste leftovers.

--- example/src/main/java/com/oviva/spicegen/spicedbbinding/internal/UpdateResultImpl.java
+++
@@ -1,5 +0,0 @@
-package com.oviva.spicegen.spicedbbinding.internal;
-
-import com.oviva.spicegen.api.UpdateResult;
-
-public record UpdateResultImpl(String consistencyToken) implements UpdateResult {}
