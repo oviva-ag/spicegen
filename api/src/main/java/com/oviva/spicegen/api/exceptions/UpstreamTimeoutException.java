924feb9772fa6c2ec91b69be7935d74d19491d5a
Rotimi Akintewe <oluwarotimi.akintewe@oviva.com>
2026-07-01 12:00:38 +0200

    CORE-6274: Add extra constructor to ensure pattern

--- api/src/main/java/com/oviva/spicegen/api/exceptions/UpstreamTimeoutException.java
+++ api/src/main/java/com/oviva/spicegen/api/exceptions/UpstreamTimeoutException.java
@@ -2,6 +2,10 @@ package com.oviva.spicegen.api.exceptions;
 
 public class UpstreamTimeoutException extends ClientException {
 
+  public UpstreamTimeoutException(String message) {
+    super(message);
+  }
+
   public UpstreamTimeoutException(String message, Throwable cause) {
     super(message, cause);
   }
